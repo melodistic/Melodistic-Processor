87af70cc5ff70cab614ce19ecdb895c580d874ea
GGolfz <52349645+GGolfz@users.noreply.github.com>
2022-12-06 23:12:44 +0700

    Update prediction_model.py

--- singleton/prediction_model.py
+++ singleton/prediction_model.py
@@ -4,5 +4,5 @@ class PredictionModel(object):
     def __new__(self):
         if not hasattr(self, 'model'):
             self.model = load_model('models/model_v1.h5')
-            self.feature_model = Model(self.model.input,self.model.layers[-6].output)
+            self.feature_model = Model(self.model.input,self.model.layers[-7].output)
         return self
